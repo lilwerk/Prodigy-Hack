ee989e66336c40aa90ecd6d1beb4357212439947
lilwerk <82842334+lilwerk@users.noreply.github.com>
2021-04-26 11:25:57 -0400

    Update Prodigy-Mod-Menu.js

--- Prodigy-Mod-Menu.js
+++ Prodigy-Mod-Menu.js
@@ -26,106 +26,106 @@ prodigydiv.remove()
 } 
 setTimeout("closeit", 3000);
 function FreeMembership(){
-PIXI.game.prodigy.player.it=true
+Phaser.GAMES[0].state.states.Login._gameObj.it=true
 }
 function FreeAtSchool(){
-PIXI.game.prodigy.classModeController.lockedZones=0
+Phaser.GAMES[0].state.states.Login._gameObj.classModeController.lockedZones=0
 }
 function TrialmastersGear() {
-PIXI.game.prodigy.player.backpack.data.outfit.push({"N":999999,"ID":24});
-PIXI.game.prodigy.player.backpack.data.boots.push({"N":999999,"ID":18});
-PIXI.game.prodigy.player.backpack.data.hat.push({"N":999999,"ID":23});
-PIXI.game.prodigy.player.backpack.data.weapon.push({"N":1,"ID":47});
+Phaser.GAMES[0].state.states.Login._gameObj.backpack.data.outfit.push({"N":999999,"ID":24});
+Phaser.GAMES[0].state.states.Login._gameObj.boots.push({"N":999999,"ID":18});
+Phaser.GAMES[0].state.states.Login._gameObj.hat.push({"N":999999,"ID":23});
+Phaser.GAMES[0].state.states.Login._gameObj.weapon.push({"N":1,"ID":47});
 }
 function GodMode() {
   GodModeDamage = prompt("How Much Do You Want To Multiply Your Damage By?") 
   GodModeHealth = prompt("How Much Do You Want To Multiply Your Health By?") 
-PIXI.game.prodigy.player.modifiers.damage=(GodModeDamage);
-PIXI.game.prodigy.player.modifiers.maxHearts=(GodModeHealth);
+Phaser.GAMES[0].state.states.Login._gameObj.modifiers.damage=(GodModeDamage);
+Phaser.GAMES[0].state.states.Login._gameObj.modifiers.maxHearts=(GodModeHealth);
 }
 function LeaveBattle() {
-PIXI.game.prodigy.debugMisc.escapeBattle();
+Phaser.GAMES[0].state.states.Login._gameObj.debugMisc.escapeBattle();
 }
 function FillEnergy() {
-PIXI.game.prodigy.debugMisc.setBattleEnergy(10);
+Phaser.GAMES[0].state.states.Login._gameObj.debugMisc.setBattleEnergy(10);
 }
 function Level1Monsters() {
-PIXI.game.prodigy.battle.constructor.MOD_DEFAULTS.level = 1;
+Phaser.GAMES[0].state.states.Login._gameObj.battle.constructor.MOD_DEFAULTS.level = 1;
 }
 function Heal() {
-PIXI.game.prodigy.player.heal(99999999999999999999999999999999999999999999999999);
+Phaser.GAMES[0].state.states.Login._gameObj.player.heal(99999999999999999999999999999999999999999999999999);
 }
 function PickLevel() {
   Level = prompt("What Level Would You Like To Be? (1-100)                                       *Must Get Into A Battle To Save Level Change");
-javascript:window.PIXI.game.prodigy.user.source.data.level=(Level);
+javascript:window.Phaser.GAMES[0].state.states.Login._gameObj.user.source.data.level=(Level);
 }
 function TeleportPlayer(e) {
 
     e = e || window.event;
 
     if (e.keyCode == '33') {
-PIXI.game.prodigy.user.x=PIXI.game.input.mousePointer.position.x
-PIXI.game.prodigy.user.y=PIXI.game.input.mousePointer.position.y
+Phaser.GAMES[0].state.states.Login._gameObj.x=Phaser.GAMES[0].state.states.Login._gameObj.input.mousePointer.position.x
+Phaser.GAMES[0].state.states.Login._gameObj.user.y=Phaser.GAMES[0].state.states.Login._gameObj.input.mousePointer.position.y
     }
 }
 document.onkeydown = TeleportPlayer;
 function setLevel() {
-  PIXI.game.prodigy.player.data.level = prompt("What level do you want to be?", "100");
+ Phaser.GAMES[0].state.states.Login._gameObj.data.level = prompt("What level do you want to be?", "100");
 }
 function levelUpPets() {
   Level = prompt("What Level Would You Like Your Pets To Be? (1-100)                          *Must Get Into A Battle To Save Level Change");
     for (let PetLoopTest = 0; PetLoopTest < PIXI.game.prodigy.player.kennel.data.length; PetLoopTest++) { 
-     PIXI.game.prodigy.player.kennel.data[PetLoopTest].level=(Level)
+     Phaser.GAMES[0].state.states.Login._gameObj.kennel.data[PetLoopTest].level=(Level)
     };
 }
 function SuperPowerfulStuff() {
-x = PIXI.game.state.states.Boot._gameData.affix
+x = Phaser.GAMES[0].state.states.Login._gameObj.state.states.Boot._gameData.affix
 for (i in x) {
-    PIXI.game.state.states.Boot._gameData.affix[i].data.value=1000
-PIXI.game.state.states.Boot._gameData.affix[i].data.valuePercent=1000.00
+    Phaser.GAMES[0].state.states.Login._gameObj.state.states.Boot._gameData.affix[i].data.value=1000
+Phaser.GAMES[0].state.states.Login._gameObj.state.states.Boot._gameData.affix[i].data.valuePercent=1000.00
 }
 }
 
 function setLocation() {
-  PIXI.game.prodigy.player.locationSelectionType = prompt("What will your location string be?", "your kitchen making a sandwich");
+  Phaser.GAMES[0].state.states.Login._gameObj.player.locationSelectionType = prompt("What will your location string be?", "your kitchen making a sandwich");
 }
 function setNickname() {
-  PIXI.game.state.states.Boot._gameData.nickname[PIXI.game.prodigy.player.name.nickname] = prompt("Set your clientside nickname (nobody else can see it but you) here.", "{first} {last}, but you can call me Einstein");
+  Phaser.GAMES[0].state.states.Login._gameObj.state.states.Boot._gameData.nickname[PIXI.game.prodigy.player.name.nickname] = prompt("Set your clientside nickname (nobody else can see it but you) here.", "{first} {last}, but you can call me Einstein");
 }
 function getallItem() {
-PIXI.game.prodigy.player.backpack.data.item=[]
-x = PIXI.game.state.states.Boot._gameData.item
+Phaser.GAMES[0].state.states.Login._gameObj.player.backpack.data.item=[]
+x = Phaser.GAMES[0].state.states.Login._gameObj.state.states.Boot._gameData.item
 for (i in x) {
-    PIXI.game.prodigy.player.backpack.data.item[i] = {"ID": x[i].ID, "N": 999999999}
+    Phaser.GAMES[0].state.states.Login._gameObj.player.backpack.data.item[i] = {"ID": x[i].ID, "N": 999999999}
 }
 }
 function getallCurrency() {
-PIXI.game.prodigy.player.backpack.data.currency=[]
-x = PIXI.game.state.states.Boot._gameData.currency
+Phaser.GAMES[0].state.states.Login._gameObj.player.backpack.data.currency=[]
+x = Phaser.GAMES[0].state.states.Login._gameObj.state.states.Boot._gameData.currency
 for (i in x) {
-    PIXI.game.prodigy.player.backpack.data.currency[i] = {"ID": x[i].ID, "N": 999999999}
-PIXI.game.prodigy.debugMisc.smallLoan(999999999);
+    Phaser.GAMES[0].state.states.Login._gameObj.backpack.data.currency[i] = {"ID": x[i].ID, "N": 999999999}
+Phaser.GAMES[0].state.states.Login._gameObj.debugMisc.smallLoan(999999999);
 }
 }
 function getallFollow() {
-PIXI.game.prodigy.player.backpack.data.follow=[]
-x = PIXI.game.state.states.Boot._gameData.follow
+Phaser.GAMES[0].state.states.Login._gameObj.backpack.data.follow=[]
+x = Phaser.GAMES[0].state.states.Login._gameObj.state.states.Boot._gameData.follow
 for (i in x) {
-    PIXI.game.prodigy.player.backpack.data.follow[i] = {"ID": x[i].ID, "N": 999999999}
+    Phaser.GAMES[0].state.states.Login._gameObj.backpack.data.follow[i] = {"ID": x[i].ID, "N": 999999999}
 }
 }
 function getallOutfit() { //so yall can shut up
-PIXI.game.prodigy.player.backpack.data.outfit=[]
-x = PIXI.game.state.states.Boot._gameData.outfit
+Phaser.GAMES[0].state.states.Login._gameObj.backpack.data.outfit=[]
+x = Phaser.GAMES[0].state.states.Login._gameObj.state.states.Boot._gameData.outfit
 for (i in x) {
-    PIXI.game.prodigy.player.backpack.data.outfit[i] = {"ID": x[i].ID, "N": 999999999}
+    Phaser.GAMES[0].state.states.Login._gameObj.backpack.data.outfit[i] = {"ID": x[i].ID, "N": 999999999}
 }
 }
 function getallRelic() { //these arent spell relics but i dont know what they are
-PIXI.game.prodigy.player.backpack.data.relic=[]
-x = PIXI.game.state.states.Boot._gameData.relic
+Phaser.GAMES[0].state.states.Login._gameObj.backpack.data.relic=[]
+x = Phaser.GAMES[0].state.states.Login._gameObj.state.states.Boot._gameData.relic
 for (i in x) {
-    PIXI.game.prodigy.player.backpack.data.relic[i] = {"ID": x[i].ID, "N": 999999999}
+    Phaser.GAMES[0].state.states.Login._gameObj.backpack.data.relic[i] = {"ID": x[i].ID, "N": 999999999}
 }
 }
 var prodigydiv = document.createElement("prodigydiv");
